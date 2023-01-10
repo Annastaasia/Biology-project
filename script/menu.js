cb1dd9dbe116a45f916782dec2ccab092f1e2e7b
katechhka <kalininak03@gmail.com>
2023-01-10 13:05:40 +0300

    хедер мобилка

---
+++ script/menu.js
@@ -0,0 +1,11 @@
+function openNav() {
+    
+    document.getElementById("ul").style.width = "280px";
+    document.getElementById("ul").style.opacity = "100";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("ul").style.width = "0";
+    document.getElementById("ul").style.opacity = "0";
+}
